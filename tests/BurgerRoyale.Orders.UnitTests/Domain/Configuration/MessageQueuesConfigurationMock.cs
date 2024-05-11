3c5f05cd8580150826f2f015e03fb006df710032
Douglas Fuelber <dev@douglasfuelber.com>
2024-05-11 18:36:37 -0300

    Background services unit tests

---
+++ tests/BurgerRoyale.Orders.UnitTests/Domain/Configuration/MessageQueuesConfigurationMock.cs
@@ -0,0 +1,16 @@
+﻿using BurgerRoyale.Orders.Domain.Configuration;
+
+namespace BurgerRoyale.Orders.UnitTests.Domain.Configuration;
+
+public static class MessageQueuesConfigurationMock
+{
+    public static MessageQueuesConfiguration Get()
+    {
+        return new MessageQueuesConfiguration {
+            OrderPaymentFeedbackQueue = "OrderPaymentFeedbackQueue",
+            OrderPaymentRequestQueue = "OrderPaymentRequestQueue",
+            OrderPreparationRequestQueue = "OrderPreparationRequestQueue",
+            OrderPreparedQueue = "OrderPreparedQueue"
+        };
+    }
+}
